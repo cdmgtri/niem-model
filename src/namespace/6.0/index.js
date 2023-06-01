5f7928a5ca9eb95e2729824e91d1c45d550499c9
CDM <cdmgtri@users.noreply.github.com>
2023-06-01 11:20:59 -0400

    Moving NDR conformance targets back to 5.0 until 6.0 versions are available

--- src/namespace/6.0/index.js
+++ src/namespace/6.0/index.js
@@ -7,8 +7,8 @@ class Namespace_6_0 extends Namespace {
 }
 
 Namespace_6_0.NDRConformanceTargets = [
-  "https://docs.oasis-open.org/niemopen/ns/specification/naming-and-design-rules/6.0/#ReferenceSchemaDocument",
-  "https://docs.oasis-open.org/niemopen/ns/specification/naming-and-design-rules/6.0/#ExtensionSchemaDocument"
+  "http://reference.niem.gov/niem/specification/naming-and-design-rules/5.0/#ReferenceSchemaDocument",
+  "http://reference.niem.gov/niem/specification/naming-and-design-rules/5.0/#ExtensionSchemaDocument"
 ];
 
 
